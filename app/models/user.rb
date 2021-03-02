af697de6a30a061fddf7602f2d8ffe227170b1da
fannyibanez <fannyibanez89@gmail.com>
2021-03-02 16:21:05 +0100

    feature : User.first.user_contact OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :user_contacts, dependent: :destroy
+  has_one :user_contact, dependent: :destroy
   has_many :flats
 end
