3a167cb4c1b65757c85664a62a38a58d3d47e9a9
fannyibanez <fannyibanez89@gmail.com>
2021-05-20 16:19:11 +0200

    booking : model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_110547) do
+ActiveRecord::Schema.define(version: 2021_05_20_140412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "guest_count"
+    t.bigint "flat_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -76,6 +88,8 @@ ActiveRecord::Schema.define(version: 2021_03_31_110547) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
   add_foreign_key "taggings", "tags"
   add_foreign_key "user_contacts", "users"
