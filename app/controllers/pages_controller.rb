a4aec7e8dd980e74e1bf372eb0da4af36256d6f6
fannyibanez <fannyibanez89@gmail.com>
2021-06-14 10:05:59 +0200

    I18n: page Home + pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @booking_by_state = current_user.bookings.group_by(&:reservation_state)
   end
 
   def dashboard
-      
-    @test_var = '<h1>Hello Var</h1>'
     @user_infos = current_user.user_contact
     @flats = current_user.flats
     @bookings = current_user.bookings
