ee0122706d67d524409f22f1d2233933f36589e3
fannyibanez <fannyibanez89@gmail.com>
2021-06-15 09:58:57 +0200

    Annotate: set up + models

--- test/models/flat_test.rb
+++ test/models/flat_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: flats
+#
+#  id              :bigint           not null, primary key
+#  address         :string
+#  description     :text
+#  name            :string
+#  price_per_night :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_flats_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require "test_helper"
 
 class FlatTest < ActiveSupport::TestCase
